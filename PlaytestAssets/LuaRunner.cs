e6ec79402e4054fce6af3375edb66013cde33ff6
VRBlue - ice tag Creator <vrblue@proton.me>
2026-04-28 20:35:14 -0500

    Add The Thing

---
+++ PlaytestAssets/LuaRunner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using XLua;
+using System.IO;
+
+public class LuaRunner : MonoBehaviour
+{
+    public string filePath;
+    private LuaEnv luaEnv;
+
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+
+        if (File.Exists(filePath))
+        {
+            string luaCode = File.ReadAllText(filePath);
+            luaEnv.DoString(luaCode);
+        }
+        else
+        {
+            Debug.LogError("Lua file not found: " + filePath);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaEnv != null)
+        {
+            luaEnv.Dispose();
+        }
+    }
+}
